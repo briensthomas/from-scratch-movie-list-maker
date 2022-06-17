1ce0f14702eecb99cb12ddfbda7bf6d5a11ba521
Brien Thomas <thomasbrien94@gmail.com>
2022-06-16 17:46:03 -0700

    starting number 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieDirector, setMovieDirector] = useState('');
+  const [movieYearReleased, setMovieYearReleased] = useState('');
+  const [movieColor, setMovieColor] = useState('');
+
+  function handleSubmit() {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieForm handleSubmit={handleSubmit} 
+        movieTitle={movieTitle}
+        setMovieTitle={setMovieTitle}
+        movieDirector={movieDirector}
+        setMovieDirector={setMovieDirector}
+        movieYearReleased={movieYearReleased}
+        setMovieYearReleased={setMovieYearReleased}
+        movieColor={movieColor}
+        setMovieColor={setMovieColor}/>
+
     </div>
   );
 }
